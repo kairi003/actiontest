f3613f339f070e2457efe476513a590e784975b3
kairi003 <kairi.satellite@gmail.com>
2024-10-03 02:25:16 +0900

    add

--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os.path
 
 from google.auth.transport.requests import Request
@@ -10,61 +11,24 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.events.owned"]
-
+CALENDAR_ID = "11156d75f2d2f07e20adc131ffc8dd5d467edda9114a6c28f357b03e16578ca2@group.calendar.google.com"
 
 def main():
   """Shows basic usage of the Google Calendar API.
   Prints the start and name of the next 10 events on the user's calendar.
   """
-  creds = None
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
   service_account_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  elif service_account_path:
-    creds = service_account.Credentials.from_service_account_file(service_account_path)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+  print(*json.load(open(service_account_path)).keys())
+  creds = service_account.Credentials.from_service_account_file(service_account_path)
 
   try:
     service = build("calendar", "v3", credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    print("Getting the upcoming 10 events")
-    events_result = (
-        service.events()
-        .list(
-            calendarId="primary",
-            timeMin=now,
-            maxResults=10,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
+    events_result = service.events().list(calendarId=CALENDAR_ID, maxResults=3).execute()
     events = events_result.get("items", [])
-
-    if not events:
-      print("No upcoming events found.")
-      return
-
-    # Prints the start and name of the next 10 events
-    for event in events:
-      start = event["start"].get("dateTime", event["start"].get("date"))
-      print(start, event["summary"])
+    print(events)
 
   except HttpError as error:
     print(f"An error occurred: {error}")
---
+++ test3.py
@@ -0,0 +1,22 @@
+from google.oauth2 import service_account
+import google.auth.transport.requests
+
+# 鍵ファイルのパスを指定
+SERVICE_ACCOUNT_FILE = 'sa.json'
+
+# 使用するスコープを指定（例：Cloud Storage）
+SCOPES = ['https://www.googleapis.com/auth/calendar.events.owned']
+
+# サービスアカウントの認証情報を取得
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# リクエストオブジェクトを作成
+request = google.auth.transport.requests.Request()
+
+# アクセストークンを取得
+credentials.refresh(request)
+access_token = credentials.token
+
+# アクセストークンを表示
+print(access_token, end='')
